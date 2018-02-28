0ddb484b2d93e51d56613dc9d1dccc9e37f07ac3
lwj1396@163.com <lwj1396@163.com>
2018-02-28 15:07:43 +0800

    threads

--- leetcodecpp/leetcodecpp/hello.cpp
+++ leetcodecpp/leetcodecpp/hello.cpp
@@ -1,15 +1,20 @@
 #include "stdafx.h"
 #include<iostream>
 #include<thread>
+#include<vector>
+#include<algorithm>
 
-void job() {
-	std::cout << "hello concurrent \n";
+void job(unsigned i) {
+	std::cout << "hello concurrent in job " << i << std::endl;
 }
 
 int main()
 {
-	std::thread t(job);
-	t.join();
+	std::vector<std::thread> threads;
+	for (unsigned i = 0; i < 20; i++) {
+		threads.push_back(std::thread(job, i));
+	}
+	std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 	char c = getchar();
 	return 0;
 }
