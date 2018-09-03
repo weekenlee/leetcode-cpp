a78b3f3d0794d9fccfbdb84dcf4dedd279ef1df5
lwj1396 <lwj1396@163.com>
2018-09-03 11:43:06 +0800

    literals.cc

---
+++ leetcodecpp/leetcodecpp/pra/literals.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Inch {
+    public:
+        explicit Inch() : Inch(0) {}
+        explicit Inch(double value_) : value(value_) {}
+
+        double toCentimeter( ) const { return value * 2.54;};
+
+    private:
+        double value = 0;
+};
+
+Inch operator "" _inch(long double arg) {
+    return Inch(arg);
+}
+
+int main() {
+    cout << (1.0_inch).toCentimeter() << "centimeter" <<endl;
+}
