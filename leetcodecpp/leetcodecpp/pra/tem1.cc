985f60870ccd420ffa9de3c30e4db82482313e99
lwj1396 <lwj1396@163.com>
2018-09-18 14:53:25 +0800

    template

--- leetcodecpp/leetcodecpp/pra/tem1.cc
+++ leetcodecpp/leetcodecpp/pra/tem1.cc
@@ -1,13 +1,21 @@
 #include<functional>
+#include<memory>
 #include<iostream>
 using namespace std;
+
+using AnyPointer = std::shared_ptr<void>;
 
 template<int repeattimes, typename F, typename ...Args>
 void doSomeThing(F &&f,  Args &&...args)
 {
+    using ResultType = typename std::result_of<F (Args...)>::type;
+
     auto func_ = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
     for (int i = 0 ; i < repeattimes; i++ ) {
-       func_();
+      ResultType result =  func_();
+      if(result) {
+        cout <<result<<endl;
+      }
     }
 }
 
@@ -21,9 +29,10 @@ int main()
     doSomeThing<5> (
             [](){
                 cout<<"hello"<<endl;
+                return 0;
             }
     );
 
-    auto fn_five = std::bind(my_divide, 10.0, 2.0);   
+    auto fn_five = std::bind(my_divide, 90.0, 2.0);   
     doSomeThing<5> (fn_five);
 }
