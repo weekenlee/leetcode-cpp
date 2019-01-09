a5a5b0a73cc16ec37abe494eea25e3fa33a13064
lwj1396 <lwj1396@163.com>
2019-01-09 18:56:47 +0800

    lua

--- con.lua
+++ con.lua
@@ -1,12 +1,17 @@
-local buff=""
-for i  = 1, 100000 do
-    buff = buff .. tostring(i)
-end
-print(buff)
+--local buff=""
+--for i  = 1, 100000 do
+--    buff = buff .. tostring(i)
+--end
+--print(buff)
+--
+--local buff={}
+--for i = 1, 100000 do
+--    buff[i]=tostring(i)
+--end
+--print(table.concat(buff))
+--
 
-local buff={}
-for i = 1, 100000 do
-    buff[i]=tostring(i)
-end
-print(table.concat(buff))
-
+local d={name="lwj",age="21"} 
+print(d[1])
+print(d["name"])
+print(d.name)
---
+++ str.lua
@@ -0,0 +1,23 @@
+print(string.gsub("a (ooo) (000) b", "%b()", ""))
+s = [[then he said: "it's all right!"]]
+a, b, c, quotedPart = string.find(s, "([\"'])(.-)%1")
+print(a)
+print(b)
+print(c)
+print(quotedPart)
+print(string.gsub("hello lua", "(%a)", "%1-%1"))
+
+s = "\\command{some text}"
+s = string.gsub(s, "\\(%a+){(.-)}", "<%1>%2<%1>")
+print(s)
+
+function expand(s)
+    return (string.gsub(s, "$(%w+)", function(n)
+        return tostring(_G[n])
+    end))
+end
+
+a = 1
+print(expand("print = $print; a = $a"))
+print(_G.a)
+print(_G['a'])
