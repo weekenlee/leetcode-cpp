58f1138cd04b1485799b4c7e61b804c8896421ad
lwj1396 <lwj1396@163.com>
2019-01-21 19:21:27 +0800

    lua

--- lua/array.c
+++ lua/array.c
@@ -9,7 +9,7 @@ typedef struct NumArray {
 } NumArray;
 
 static int newarray(lua_State *L) {
-    int n = luaL_checkint(L, 1);
+    int n = luaL_checknumber(L, 1);
     size_t  nbytes = sizeof(NumArray) + (n-1)*sizeof(double);
     NumArray *a = (NumArray *)lua_newuserdata(L, nbytes);
     a->size = n;
@@ -18,7 +18,7 @@ static int newarray(lua_State *L) {
 
 static int setarray(lua_State *L) {
     NumArray *a = (NumArray*)lua_touserdata(L, 1);
-    int index = luaL_checkint(L, 2);
+    int index = luaL_checknumber(L, 2);
     double value = luaL_checknumber(L, 3);
 
     a->values[index - 1] = value;
@@ -27,7 +27,7 @@ static int setarray(lua_State *L) {
 
 static int getarray(lua_State *L) {
     NumArray *a = (NumArray *)lua_touserdata(L, 1);
-    int index = luaL_checkint(L, 2);
+    int index = luaL_checknumber(L, 2);
     lua_pushnumber(L, a->values[index - 1]);
     return 1;
 }
@@ -40,6 +40,6 @@ static const struct luaL_Reg arraylib[] = {
 };
 
 int luaopen_array(lua_State *L) {
-    luaL_openlib(L, "array", arraylib, 0);
+    luaL_newlib(L, arraylib);
     return 1;
 }
