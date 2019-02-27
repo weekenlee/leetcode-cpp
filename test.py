a739bcdda747c5b6f6e75873580f94a4122e0b71
lwj1396@163.com <lwj1396@163.com>
2019-02-27 18:46:51 +0800

    t

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import time
 
 bot = Bot(cache_path=True, console_qr=True)
 my_friend = bot.friends().search(u'小以')[0]
-my_friend2 = bot.friends().search(u'vklee')[0]
-
+my_self = bot.friends().search(u'vklee')[0]
+print(u"启动")
 
 @bot.register(my_friend)
 def reply_my_friend(msg):
@@ -15,19 +15,21 @@ def reply_my_friend(msg):
         msg.chat.send(u'已收到您的消息, 请稍后')
 
 def sayhi():
-    print("发出消息", time.strftime('%H:%M:%S',time.localtime(time.time())))
-    my_friend.send(u'20分钟到啦, 起来运动运动啦!')
+    now = time.strftime('%H:%M:%S',time.localtime(time.time()))
+    h = now.split(":")[0]
+    if (int(h) > 9 and int(h) < 12) or (int(h)>14 and int(h)<18):
+        print("发出消息", )
+        my_friend.send(u'20分钟到啦, 起来运动运动啦!')
 
 def saymyself():
     print("发出消息", time.strftime('%H:%M:%S',time.localtime(time.time())))
-    my_friend2.send(u'hh')
+    my_self.send(u'hh')
 
 sayhi()
 saymyself()
 
 schedule.every(20).minutes.do(sayhi)
 schedule.every(8).minutes.do(saymyself)
-print(u"启动")
 
 while True:
     schedule.run_pending()
