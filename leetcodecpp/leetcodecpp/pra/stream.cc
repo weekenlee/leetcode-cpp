a45296d1a348e420e8d3db049f0c1916301a02d8
lwj1396 <lwj1396@163.com>
2019-03-11 14:33:15 +0800

    stream

---
+++ leetcodecpp/leetcodecpp/pra/stream.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+struct Date {
+    int month;
+    int year;
+};
+
+istream& operator >> (istream &is , Date& d) {
+    is >> d.month;
+    char dash;
+    is >> dash;
+    if(dash != '-') {
+        is.setstate(ios::failbit);
+    }
+    is >> d.year;
+}
+
+int main() {
+   Date d;
+   cin >> d;
+
+   cout << d.month<< endl;
+   cout << d.year<< endl;
+}
