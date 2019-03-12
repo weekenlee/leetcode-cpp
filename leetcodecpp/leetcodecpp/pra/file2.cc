08d30dc44575b5d7f034d2dc88c13ef644e0560d
lwj1396 <lwj1396@163.com>
2019-03-12 10:36:02 +0800

    file

---
+++ leetcodecpp/leetcodecpp/pra/file2.cc
@@ -0,0 +1,22 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+int main() {
+    const int SZ = 100;
+    char buf[SZ];
+    ifstream in("file.cc");
+    while(in.getline(buf, SZ)) {
+        cout << buf ;
+    }
+    cout << endl;
+    char c;
+    ifstream in2("file.cc");
+    while(in2>>c) {
+        cout <<c;
+    }
+    cout << endl;
+    ifstream in3("file.cc");
+    cout << in3.rdbuf();
+    return 0;
+}
