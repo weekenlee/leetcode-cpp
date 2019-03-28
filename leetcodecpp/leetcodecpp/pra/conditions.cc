399a75dd61c8727711e8a772e7f6ef9b476f8530
lwj1396 <lwj1396@163.com>
2019-03-28 14:17:03 +0800

    template

---
+++ leetcodecpp/leetcodecpp/pra/conditions.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template<bool cond> struct Select{};
+
+template<>
+class Select<true> {
+    static void statment1() {
+        cout << "statement1" <<endl;
+     }
+    public:
+    static void f() {statment1();}
+} ;
+
+template<>
+class Select<false> {
+    static void statement2() {
+        cout <<"statement2" <<endl;
+    }
+    public:
+    static void f() {statement2();}
+};
+
+template<bool cond> void execute() {
+    Select<cond>::f();
+}
+
+int main() {
+    execute<sizeof(int) == 4>();
+}
+
+
