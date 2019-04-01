f5fbc5eee7d8c9e1c3641a387a29d74a244f81dd
lwj1396 <lwj1396@163.com>
2019-04-01 13:31:25 +0800

    functional

---
+++ leetcodecpp/leetcodecpp/pra/ptrfunc1.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int d[]= {123, 94, 10, 314, 315};
+const int DSZ = sizeof d / sizeof d[0];
+bool isEven(int x) {return x % 2 == 0;}
+
+int main() {
+    vector<bool> vb;
+    transform(d, d+DSZ, back_inserter(vb), not1(ptr_fun(isEven)));
+    copy(vb.begin(), vb.end(), ostream_iterator<bool>(cout, " "));
+    vb.clear();
+    cout <<endl;
+    transform(d, d+DSZ, back_inserter(vb), ptr_fun(isEven));
+    copy(vb.begin(), vb.end(), ostream_iterator<bool>(cout, " "));
+    cout <<endl;
+}
+
