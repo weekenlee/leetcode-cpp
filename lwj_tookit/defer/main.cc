3e144ede4580d49aeb311f1377b179c0dd71e105
lwj1396 <lwj1396@163.com>
2019-05-06 16:30:16 +0800

    go

--- lwj_tookit/defer/main.cc
+++ lwj_tookit/defer/main.cc
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <memory>
 #include "defer.hpp"
 
 using namespace std;
 using namespace lwj;
 
 void test() {
+
     defer defer_exit, defer_fail(defer::execution::exception);
 
     defer_exit += [](){ cout << "defer end"<<endl;};
-    defer_fail += [](){ cout << "defer fail"<<endl;};
+    defer_fail += [](){ cout << "defer fail"<<endl; throw 1;};
 
     defer_exit += [](){ cout << "defer end2"<<endl;};
     defer_fail += [](){ cout << "defer fail2"<<endl;};
     
     cout << "do thing..."<<endl;
 
-    throw 1;
+
 }
 
 
 int main() {
+
+    shared_ptr<int> x(NULL, [&](int *) { cout << "defer by smart pointer"<<endl; });
+
     test();
 }
