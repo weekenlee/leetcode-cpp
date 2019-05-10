209454f2be1294da482a223d150b0fabb5f0a23e
lwj1396@163.com <lwj1396@163.com>
2019-05-10 14:28:46 +0800

    nodejsaddon

--- lwj_tookit/defer/main.cc
+++ lwj_tookit/defer/main.cc
@@ -6,7 +6,6 @@ using namespace std;
 using namespace lwj;
 
 void test() {
-
     defer defer_exit, defer_fail(defer::execution::exception);
 
     defer_exit += [](){ cout << "defer end"<<endl;};
@@ -16,14 +15,12 @@ void test() {
     defer_fail += [](){ cout << "defer fail2"<<endl;};
     
     cout << "do thing..."<<endl;
-
-
 }
 
 
 int main() {
-
+    //利用智能指针来进行
     shared_ptr<int> x(NULL, [&](int *) { cout << "defer by smart pointer"<<endl; });
-
+    //利用自定义类来进行
     test();
 }
