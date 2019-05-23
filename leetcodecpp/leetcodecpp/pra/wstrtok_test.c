54c1c70a7ec4213ffffe4797481eb67ee48368f3
lwj1396@163.com <lwj1396@163.com>
2019-05-23 14:41:07 +0800

    wstring

--- leetcodecpp/leetcodecpp/pra/wstrtok_test.c
+++ leetcodecpp/leetcodecpp/pra/wstrtok_test.c
@@ -1,9 +1,18 @@
 #include <wchar.h>
 #include <stdio.h>
+#include <locale.h>
 
 int main(void)
 {
+    setlocale(LC_ALL, "");
     wchar_t input[] = L"你好啊哈哈啊诶呀啊";
-    wprintf(L"before: %ls", input);
+    printf("before: %ls\n", input);
+
+    wchar_t *buffer;
+    wchar_t *token = wcstok(input, L"啊", &buffer);
+    while(token) {
+        printf("%ls\n", token);
+        token = wcstok(NULL, L"啊", &buffer);
+    }
     return 0;
 }
